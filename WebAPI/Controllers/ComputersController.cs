5f8bd91a064c4e622f7138f4aaad463b97298dc4
Wayde Reitsma <102506005@student.swin.edu.au>
2019-11-29 09:40:54 +1100

    Add computers endpoint

---
+++ WebAPI/Controllers/ComputersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputersController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Computer> GetAll([FromServices] IConfiguration configuration)
+        {
+            using (var connection = new SqlConnection(configuration.GetConnectionString("Database")))
+            {
+                connection.Open();
+
+                var command = new SqlCommand($"SELECT * FROM [Computer]", connection);
+
+                var reader = command.ExecuteReader();
+
+                var computers = new List<Computer>();
+
+                while(reader.Read())
+                {
+                    computers.Add(new Computer(
+                        (int)reader["Number"],
+                        (int)reader["AssembledYear"],
+                        (string)reader["Building"],
+                        (int)reader["RoomNo"]
+                    ));
+                }
+
+                reader.Close();
+
+                return computers;
+            }
+        }
+    }
+}
