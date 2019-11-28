9237ea135c7893416134b3d9c2b29541db55c1c2
Wayde Reitsma <102506005@student.swin.edu.au>
2019-11-29 09:36:31 +1100

    Add class/{classCode} endpoint

---
+++ WebAPI/Controllers/ClassController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        [HttpGet("{classCode}")]
+        public ActionResult<Class> Get([FromServices] IConfiguration configuration, string classCode)
+        {
+            using (var connection = new SqlConnection(configuration.GetConnectionString("Database")))
+            {
+                connection.Open();
+
+                var command = new SqlCommand($"SELECT * FROM [Class] C where C.ClassCode = '{classCode}'", connection);
+
+                var reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    var class_ = new Class(
+                        (string)reader["ClassCode"],
+                        (string)reader["Name"],
+                        (string)reader["Building"],
+                        (int)reader["RoomNo"]
+                    );
+
+                    reader.Close();
+
+                    return class_;
+                }
+                else
+                {
+                    reader.Close();
+
+                    return NotFound();
+                }
+            }
+        }
+    }
+}
